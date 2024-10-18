62f85fdea431fe7fed6578abeafc9e0942da9a46
Nicksubash <nicksubash9@gmail.com>
2024-10-18 22:03:23 +0900

    first commit

---
+++ app/components/navbar/page.js
@@ -0,0 +1,14 @@
+import react from "react";
+
+export default function Navbar() {
+    return (
+      <>
+      <div>
+        <h1 className="text-2xl ">
+        Login
+        </h1>
+        
+      </div>
+      </>
+    );
+  }
